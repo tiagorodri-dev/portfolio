6540f21a7e6d3e1407cce014b4d23f0dcd60bd3e
unknown <tiagoarodrigues.dev@gmail.com>
2024-10-28 16:39:59 -0300

    style: construct section aboutMe

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,16 +1,33 @@
-import Title from "../Title"
+import Title from "../Title";
+import imageAboutMe from "../../assets/img_formatura.jpg";
 import "./style.css";
 
 function AboutMe() {
     return (
         <section className="section-sobre-mim">
-            <Title name="Sobre mim"/>
+            <Title name="Sobre mim" />
 
             <section className="sobre">
+                <div>
+                    <img src={imageAboutMe} className="imageAboutMe" alt="Imagem da minha formatura" />
+                </div>
 
+                <div className="sobre-mim">
+                    <span>
+                        Desenvolvedor de Software com mais de 2 anos de experiência e graduado em Análise e Desenvolvimento de Sistemas.
+                        Tenho experiência em desenvolver soluções inovadoras nas áreas de Telecomunicações e Propriedade Intelectual,
+                        sempre com foco em otimizar processos e garantir alta performance.
+                    </span>
+                    <span>
+                        Acredito na importância de uma comunicação clara e colaborativa com clientes para entender suas necessidades
+                        e criar soluções personalizadas que gerem valor. Se você busca um desenvolvedor dedicado e focado
+                        em resultados, estou aqui para transformar sua ideia em realidade.
+                    </span>
+                    <p>Interessado em colaborar ou trazer suas ideias para o mundo digital? <a href="#contato">Vamos conversar!</a></p>
+                </div>
             </section>
         </section>
-    )
+    );
 }
 
-export default AboutMe
+export default AboutMe;
