530c5763422db8ee293c320c24921592c2a954f5
unknown <tiagoarodrigues.dev@gmail.com>
2025-04-30 11:03:44 -0300

    style: improvment presentation of technologies

--- src/components/sections/Technologies/index.jsx
+++ src/components/sections/Technologies/index.jsx
@@ -1,3 +1,4 @@
+import AnimatedSection from "../../ui/AnimatedSection";
 import Title from "../../ui/Title";
 import "./style.css";
 
@@ -5,83 +6,87 @@ function Technologies() {
   const technologies = [
     {
       id: 1,
-      name: "Typescript",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    },
+
+    {
+      id: 2,
       image:
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
     },
 
     {
-      id: 2,
-      name: "React",
+      id: 3,
       image:
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
     },
 
     {
-      id: 3,
-      name: "NodeJS",
-      image:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
-    },
-
-    {
       id: 4,
-      name: "Git",
-      image:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
     },
 
     {
       id: 5,
-      name: "HTML",
       image:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg",
     },
 
     {
       id: 6,
-      name: "CSS",
       image:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain-wordmark.svg",
     },
 
     {
       id: 7,
-      name: "Bootstrap",
       image:
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
     },
 
     {
       id: 8,
-      name: "Javascript",
       image:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
     },
 
     {
       id: 9,
-      name: "Firebase",
       image:
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg",
+    },
+
+    {
+      id: 10,
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+    },
+
+    {
+      id: 11,
+      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
     },
   ];
 
   return (
-    <section className="session-knowledge">
-      <Title name="Tecnologias" />
+    <section className="session-technology">
+      <div className="technologies-wrapper">
+        <AnimatedSection>
+          <Title name="Tecnologias e Ferramentas" />
 
-      <div className="knowledges">
-        {technologies.map((stack) => (
-          <div className="knowledge" key={stack.id}>
-            <img
-              className="image-knowledge"
-              src={stack.image}
-              alt={stack.name}
-            />
-            <h4 className="text-knowledge">{stack.name}</h4>
+          <div className="technologies">
+            {technologies.concat(technologies).map((stack) => (
+              <div className="technology" key={stack.id}>
+                <img
+                  className="image-technology"
+                  src={stack.image}
+                  alt="Tecnologia"
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        </AnimatedSection>
       </div>
     </section>
   );
