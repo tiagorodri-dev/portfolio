3c5c2d2bbad0cef899adc6614de960edb9e03e05
unknown <tiagoarodrigues.dev@gmail.com>
2025-05-16 10:47:18 -0300

    style: improving text and image on the home page, aiming to increase performance

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,50 +1,10 @@
-import { useState, useEffect } from "react";
-import capa from "../../../assets/capa.webp";
 import Menu from "../Navbar";
-import ArrowButton from "../../ui/ArrowDownButton";
+import scrollToSection from "../../../utils/scrollToSection";
 import ParticleBackground from "../../shared/ParticleBackground/ParticleBackground";
+import capa from "../../../assets/capa.webp";
 import "./style.css";
 
 function Header() {
-  const [text, setText] = useState("");
-  const [index, setIndex] = useState(0);
-  const roles = [
-    "",
-    "Desenvolvedor Web",
-    "Analista de Sistemas",
-    "Desenvolvedor Front-end",
-    "Desenvolvedor Fullstack",
-  ];
-
-  useEffect(() => {
-    let typingTimeout;
-
-    const type = (currentCharIndex = 0) => {
-      const currentText = roles[index];
-
-      if (currentCharIndex < currentText.length) {
-        setText((prev) => prev + currentText[currentCharIndex]);
-        typingTimeout = setTimeout(() => type(currentCharIndex + 1), 150);
-      } else {
-        typingTimeout = setTimeout(() => erase(currentText.length), 1000);
-      }
-    };
-
-    const erase = (currentCharIndex) => {
-      if (currentCharIndex > 0) {
-        setText((prev) => prev.slice(0, -1));
-        typingTimeout = setTimeout(() => erase(currentCharIndex - 1), 100);
-      } else {
-        setIndex((prev) => (prev + 1) % roles.length);
-        setText("");
-        typingTimeout = setTimeout(() => type(), 500);
-      }
-    };
-
-    type();
-
-    return () => clearTimeout(typingTimeout);
-  }, [index]);
 
   return (
     <header className="header-bg" id="inicio">
@@ -53,23 +13,24 @@ function Header() {
 
       <div className="banner container">
         <div className="banner-content">
+          <h1>Consultoria <span className="destaque">profissional</span> para o seu software e aplicativo</h1>
           <p>
-            Eu sou <span className="destaque">Tiago Alves Rodrigues</span> e
-            atuo como
+            Sou Tiago Alves Rodrigues, Analista de Sistemas
+            focado na otimização de processos através do desenvolvimento de soluções inovadoras e intuitivas.
           </p>
-          <h1>{text}</h1>
-          <p className="descricao">
-            Desenvolvendo soluções inovadoras e criando experiências marcantes
-            para os usuários
-          </p>
+          <a
+            className="button-conhecer"
+            href="#sobre"
+            onClick={(e) => scrollToSection(e, "sobre")}
+          >
+            Conheça mais
+          </a>
         </div>
 
         <div className="banner-img">
-          <img src={capa} alt="Tiago" loading="lazy" />
+          <img src={capa} alt="Tiago" loading="lazy" width={350} />
         </div>
       </div>
-
-      <ArrowButton />
     </header>
   );
 }
